b37983f6d84849408ee29043a636a73345b5c50b
James Lawrence <jlawrence@datto.com>
2016-02-09 22:06:19 -0500

    remove old code

--- genieql.go
+++ genieql.go
@@ -25,46 +25,6 @@ func ConnectDB() (*sql.DB, error) {
 	return sql.Open(dbconf.Driver, fmt.Sprintf("user=%s dbname=%s sslmode=%s port=%d", dbconf.User, dbconf.Name, dbconf.SSLMode, dbconf.Port))
 }
 
-// Mapping - structure that holds a mapping of the aliases to fields
-type Mapping struct {
-	Package string
-	Type    string
-	Fields  []Field
-}
-
-func (t Mapping) FindMatch(column string) (Field, bool) {
-	for _, field := range t.Fields {
-		if field.IsAlias(column) {
-			return field, true
-		}
-	}
-
-	return Field{}, false
-}
-
-type Field struct {
-	Type        string
-	StructField string
-	Aliases     []string
-}
-
-func (t Field) IsAlias(s string) bool {
-	for _, alias := range t.Aliases {
-		if alias == s {
-			return true
-		}
-	}
-
-	return false
-}
-
-type Match struct {
-	Mapping
-	ArgPosition int
-	Field
-	ScanPosition int
-}
-
 // Preface text inserted at the top of all generated files.
 const Preface = `
 
