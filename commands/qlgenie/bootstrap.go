8294f30c51a064d7c360d544b72a40f2a5baa6bc
James Lawrence <jlawrence@datto.com>
2016-02-09 21:53:52 -0500

    documentation update

--- commands/qlgenie/bootstrap.go
+++ commands/qlgenie/bootstrap.go
@@ -8,8 +8,8 @@ import (
 
 // data stored in qlgenie.conf - dialect, default alias strategy, map definition directory,
 // default for including table prefix aliases, database connection information.
-// qlgenie bootstrap psql://host:port/example?username=x&password=y -> creates example.qlgenie.
-// qlgenie bootstrap --ouput="someothername.qlgenie" psql://host:port/example?username=x&password=y -> creates someothername.qlgenie
+// qlgenie bootstrap psql://host:port/example?username=123&password=456 -> creates example.qlgenie.
+// qlgenie bootstrap --ouput="someothername.qlgenie" psql://host:port/example?username=123&password=456 -> creates someothername.qlgenie
 type bootstrap struct {
 	outputfilepath string
 	dburi          *url.URL
