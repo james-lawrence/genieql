abcf776b1fb4e448ddc3acebf2f4ff6495bfa6b4
James Lawrence <jljatone@gmail.com>
2016-08-28 09:39:04 -0400

    cleanup rebuilding of test db

--- scanner/internal/examples/type1.go
+++ scanner/internal/examples/type1.go
@@ -2,6 +2,8 @@ package examples
 
 import "time"
 
+//go:generate dropdb --if-exists -U postgres genieql_test_template
+//go:generate createdb -U postgres genieql_test_template
 //go:generate psql -1 -f structure.sql genieql_test_template
 //go:generate genieql bootstrap --driver=github.com/lib/pq --output-file=scanner-test.config postgres://$USER@localhost:5432/genieql_test_template?sslmode=disable
 //go:generate genieql map --config=scanner-test.config bitbucket.org/jatone/genieql/scanner/internal/examples.Type1
