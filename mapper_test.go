246b01d84052edc4f1e1ed3af829e3e031362f9f
James Lawrence <jlawrence@datto.com>
2017-02-04 16:03:07 -0500

    test rework

--- mapper_test.go
+++ mapper_test.go
@@ -6,32 +6,24 @@ import (
 	. "bitbucket.org/jatone/genieql"
 
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+
+	"bitbucket.org/jatone/genieql/astutil"
 )
 
 var _ = Describe("Mapper", func() {
-	Describe("MapFieldToColumn", func() {
-		examples := []struct {
-			arg    *ast.Ident
-			column string
-			field  *ast.Field
-			offset int
-			Aliaser
-		}{
-			{
-				arg:     &ast.Ident{Name: "arg0"},
-				column:  "column1",
-				field:   &ast.Field{Names: []*ast.Ident{&ast.Ident{Name: "Column1"}}, Type: &ast.Ident{Name: "int"}},
-				offset:  0,
-				Aliaser: AliasStrategyCamelcase,
-			},
-		}
-
-		It("should return true if the column matches the field and its aliases", func() {
-			for _, example := range examples {
-				matchFound := MapFieldToColumn(example.column, example.offset, example.field, example.Aliaser)
-				Expect(matchFound).ToNot(BeNil())
-			}
-		})
-	})
+	DescribeTable("MapFieldToColumn",
+		func(column string, field *ast.Field, offset int, aliaser Aliaser) {
+			matchFound := MapFieldToColumn(column, offset, field, aliaser)
+			Expect(matchFound).ToNot(BeNil())
+		},
+		Entry(
+			"example 1 - simple match",
+			"column1",
+			astutil.Field(ast.NewIdent("int"), ast.NewIdent("Column1")),
+			0,
+			AliasStrategyCamelcase,
+		),
+	)
 })
