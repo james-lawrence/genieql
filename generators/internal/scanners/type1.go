5ad929370f2e4b69e9e2cfce9f5bf82d903bc441
James Lawrence <jljatone@gmail.com>
2017-07-15 13:47:11 -0400

    cleanup scanner test package generate commands

--- generators/internal/scanners/type1.go
+++ generators/internal/scanners/type1.go
@@ -2,10 +2,10 @@ package scanners
 
 import "time"
 
-//go:generate genieql map --config=generators-test.config bitbucket.org/jatone/genieql/generators/internal/scanners.Type1
-//go:generate genieql scanner default --config=generators-test.config --output=type1_scanner.gen.go bitbucket.org/jatone/genieql/generators/internal/scanners.Type1 type1
-//go:generate genieql scanner dynamic --config=generators-test.config --output=type1_dynamic_scanner.gen.go bitbucket.org/jatone/genieql/generators/internal/scanners.Type1 type1
-//go:generate genieql generate crud --config=generators-test.config --output=type1_queries.gen.go bitbucket.org/jatone/genieql/generators/internal/scanners.Type1 type1
+//go:generate genieql map --config=generators-test.config Type1
+//go:generate genieql scanner default --config=generators-test.config --output=type1_scanner.gen.go Type1 type1
+//go:generate genieql scanner dynamic --config=generators-test.config --output=type1_dynamic_scanner.gen.go Type1 type1
+//go:generate genieql generate crud --config=generators-test.config --output=type1_queries.gen.go Type1 type1
 
 // Type1 for testing
 type Type1 struct {
