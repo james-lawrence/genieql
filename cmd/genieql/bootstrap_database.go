8a4a281fe786d6d83dfd8073c6cfbf9916ec7cce
James Lawrence <jljatone@gmail.com>
2021-03-06 05:27:08 -0500

    update default driver for db bootstrap to pgx

--- cmd/genieql/bootstrap_database.go
+++ cmd/genieql/bootstrap_database.go
@@ -38,7 +38,7 @@ func (t *bootstrapDatabase) configure(bootstrap *kingpin.CmdClause) *kingpin.Cmd
 	bootstrap.Flag("output-directory", "directory to place the configuration file").Default(genieql.ConfigurationDirectory()).StringVar(&t.outputfilepath)
 	bootstrap.Flag("output-file", "filename of the configuration directory").Default("default.config").StringVar(&t.outputfile)
 	bootstrap.Flag("driver", "name of the underlying driver for the database, usually the import url").
-		Default("github.com/lib/pq").StringVar(&t.driver)
+		Default("github.com/jackc/pgx").StringVar(&t.driver)
 	bootstrap.Flag("queryer", "the default queryer to use").Default("*sql.DB").StringVar(&t.queryer)
 	bootstrap.Flag("rowtype", "the default type to use for retrieving rows").Default("*sql.Row").StringVar(&t.rowtype)
 	bootstrap.Arg("uri", "uri for the database qlgenie will work with").Required().URLVar(&t.dburi)
