c814690c4830297e98825332dd601bee776cf09c
James Lawrence <jljatone@gmail.com>
2022-12-15 15:03:53 -0500

    remove deprecated examples

--- examples/definitions/doc.go
+++
@@ -1,5 +0,0 @@
-// Package definitions - this package contains examples of definition files
-//
-// definition files are valid go files that tell genieql what to generate
-// for code.
-package definitions
--- examples/definitions/genieql.structs.query.go
+++
@@ -1,6 +0,0 @@
-//go:build genieql && generate && structure && query
-// +build genieql,generate,structure,query
-
-package definitions
-
-const QueryExample1, QueryExample2 = "SELECT uuid_field FROM example1", "SELECT uuid_field FROM example2"
