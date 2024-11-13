03f8b82c2615d445564d3ddaeb2b9ec98e734f30
James Lawrence <jljatone@gmail.com>
2024-11-13 09:48:15 -0500

    implement ci/cd

---
+++ .eg/go.mod
@@ -0,0 +1,5 @@
+module eg/compute
+
+go 1.23.2
+
+require github.com/egdaemon/eg v0.0.0-20241112184001-8a284259f6a0
---
+++ .eg/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/egdaemon/eg/runtime/wasi/eg"
+	"github.com/egdaemon/eg/runtime/wasi/egenv"
+	"github.com/egdaemon/eg/runtime/wasi/eggit"
+	"github.com/egdaemon/eg/runtime/wasi/eggolang"
+)
+
+func main() {
+	ctx, done := context.WithTimeout(context.Background(), egenv.TTL())
+	defer done()
+
+	err := eg.Perform(
+		ctx,
+		eggit.AutoClone,
+		eggolang.AutoCompile(),
+		eggolang.AutoTest(),
+	)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
