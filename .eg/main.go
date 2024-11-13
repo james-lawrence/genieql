9abfb9675a9375fd44341ad48d9ebd857240a1d9
James Lawrence <jljatone@gmail.com>
2024-11-13 09:48:15 -0500

    implement ci/cd

--- .eg/main.go
+++ .eg/main.go
@@ -2,23 +2,51 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/egdaemon/eg/runtime/wasi/eg"
 	"github.com/egdaemon/eg/runtime/wasi/egenv"
 	"github.com/egdaemon/eg/runtime/wasi/eggit"
-	"github.com/egdaemon/eg/runtime/wasi/eggolang"
+	"github.com/egdaemon/eg/runtime/wasi/shell"
+	"github.com/egdaemon/eg/runtime/x/wasi/eggolang"
 )
+
+func Debug(ctx context.Context, id eg.Op) error {
+	return shell.Run(
+		ctx,
+		shell.New("echo ${PATH}"),
+	)
+}
+
+func Setup(ctx context.Context, id eg.Op) error {
+	runtime := shell.Runtime().
+		Environ("PATH", fmt.Sprintf("%s:/root/go/bin", os.Getenv("PATH"))).
+		Environ("GOBIN", "/usr/local/bin").
+		Environ("USER", "root")
+	return shell.Run(
+		ctx,
+		runtime.New("pg_isready").Attempts(15), // 15 attempts = ~3seconds
+		runtime.New("go env"),
+		runtime.New("su postgres -l -c 'psql --no-psqlrc -U postgres -d postgres -c \"CREATE ROLE root WITH SUPERUSER LOGIN\"'"),
+		runtime.New("go generate ./... && go fmt ./..."),
+	)
+}
 
 func main() {
 	ctx, done := context.WithTimeout(context.Background(), egenv.TTL())
 	defer done()
 
+	c1 := eg.Container("genieql.ubuntu.22.04")
+
 	err := eg.Perform(
 		ctx,
 		eggit.AutoClone,
-		eggolang.AutoCompile(),
-		eggolang.AutoTest(),
+		eg.Build(
+			c1.BuildFromFile(".eg/Containerfile"),
+		),
+		eg.Module(ctx, c1, Debug, eggolang.AutoCompile(), Setup, eggolang.AutoTest()),
 	)
 
 	if err != nil {
